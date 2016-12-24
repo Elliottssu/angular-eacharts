ad6b9e287fe56d27cdc822feb3cd0ff6442dadba
sulianggang <sulianggang@askingdata.com>
2016-12-24 17:35:41 +0800

    updata 图

--- public/javascripts/echartsController.js
+++ public/javascripts/echartsController.js
@@ -2,7 +2,7 @@ angular.module('echartsCtrlModule', ['echartsServiceModule'])
 	.controller('echartsCtrl', function ($scope, $http, echartService) {
 		$scope.showTrend = function (index) {
 		  $scope.showId = [];
-		  $scope.showId[index] = true;
+		  $scope.showId[index] = [true, false, false, false, true, false, false, false];
 		  switch (index) {
 		    case 0:
 		      $scope.lineSingleOption = $scope.lineOption1;
@@ -16,6 +16,18 @@ angular.module('echartsCtrlModule', ['echartsServiceModule'])
 		    case 3:
 		      $scope.lineSingleOption = $scope.lineOption4;
 		      break;
+			case 4:
+			  $scope.lineSingleOption2 = $scope.lineOption5;
+			  break;
+			case 5:
+			  $scope.lineSingleOption2 =$scope.lineOption6;
+			  break;
+			case 6:
+		      $scope.lineSingleOption2 = $scope.lineOption7;
+			  break;
+			case 7:
+			  $scope.lineSingleOption2 = $scope.lineOption8;
+			  break;
 		  }
 		};
 
@@ -33,24 +45,50 @@ angular.module('echartsCtrlModule', ['echartsServiceModule'])
 		$scope.lineData4 = {
 			legend: ['评价','增长率']
 		}
+
+		$scope.lineData5 = {
+			legend: ['销售量','增长率']
+		}
+		$scope.lineData6 = {
+			legend: ['销售额','增长率']
+		}
+		$scope.lineData7 = {
+			legend: ['收藏','增长率']
+		}
+		$scope.lineData8 = {
+			legend: ['评价','增长率']
+		}
+
 		$http.get('./resources/goodsInfo.json').success(function (data) {
 			echartService.handleData(data, $scope.lineData1, 'amount', 'amountRatio')
-			$scope.lineOption1 = echartService.typeLine($scope.lineData1)
-		    $scope.lineSingleOption = $scope.lineOption1
+			$scope.lineOption1 = echartService.typeLineOption($scope.lineData1)
 
 	    	echartService.handleData(data, $scope.lineData2, 'sale', 'saleRatio')
-	    	$scope.lineOption2 = echartService.typeLine($scope.lineData2)
-	        $scope.lineSingleOption = $scope.lineOption1	
+	    	$scope.lineOption2 = echartService.typeLineOption($scope.lineData2)
 
         	echartService.handleData(data, $scope.lineData3, 'focusNum', 'commentNumRatio')
-        	$scope.lineOption3 = echartService.typeLine($scope.lineData3)
-            $scope.lineSingleOption = $scope.lineOption1
+        	$scope.lineOption3 = echartService.typeLineOption($scope.lineData3)
 
         	echartService.handleData(data, $scope.lineData4, 'commentNum', 'amountRatio')
-        	$scope.lineOption4 = echartService.typeLine($scope.lineData4)
+        	$scope.lineOption4 = echartService.typeLineOption($scope.lineData4)
+
             $scope.lineSingleOption = $scope.lineOption1
 
 
+            //第二种
+			echartService.handleData(data, $scope.lineData5, 'amount', 'amountRatio')
+			$scope.lineOption5 = echartService.typeBarLineOption($scope.lineData5)
+
+	    	echartService.handleData(data, $scope.lineData6, 'sale', 'saleRatio')
+	    	$scope.lineOption6 = echartService.typeBarLineOption($scope.lineData6)
+
+	    	echartService.handleData(data, $scope.lineData7, 'focusNum', 'commentNumRatio')
+	    	$scope.lineOption7 = echartService.typeBarLineOption($scope.lineData7)
+
+	    	echartService.handleData(data, $scope.lineData8, 'commentNum', 'amountRatio')
+	    	$scope.lineOption8 = echartService.typeBarLineOption($scope.lineData8)
+
+	        $scope.lineSingleOption2 = $scope.lineOption5
 		})
 
 	})
--- public/javascripts/echartsService.js
+++ public/javascripts/echartsService.js
@@ -14,8 +14,8 @@ angular.module('echartsServiceModule', [])
 			return afterData
 		}
 
-		//折线图
-		service.typeLine = function (data) {
+		//单折线图
+		service.typeLineOption = function (data) {
 			var option = {
 	            tooltip: {},
 	            legend: {
@@ -41,5 +41,108 @@ angular.module('echartsServiceModule', [])
 			
 			return option
 		}
+
+		//柱状与折线图
+		service.typeBarLineOption = function (data) {
+		  var option = {
+		          tooltip: {
+		            trigger: "axis"
+		          },
+		          legend: {
+		            show: true,
+		            data: data.legend,
+		            top: 10
+		          },
+		          height: "70%",
+		          grid: {
+		            bottom: 15,
+		            left: 20,
+		            right: 20
+		          },
+		          color : ['#39CCCC','#0074D9'],
+		          xAxis: [
+		            {
+		              type: "category",
+		              nameGap: 35,
+		              axisTick: {
+		                show: false
+		              },
+		              axisLine: {
+		                onZero: true
+		              },
+		              data: data.xAxisData
+		            }
+		          ],
+		          yAxis: [
+		            {
+		              type: "value",
+		              name: data.legend[0],
+		              axisLabel: {
+		                formatter: "{value}",
+		                inside: true,
+		                margin: 8
+		              },
+		              axisTick: {
+		                show: false
+		              },
+		              nameTextStyle: {
+		                color: "#000"
+		              },
+		              zlevel: 10,
+		              splitLine: {
+		                show: false
+		              },
+		              splitArea: {
+		                show: false
+		              }
+		            },
+		            {
+		              type: "value",
+		              name: data.legend[1],
+		              axisLabel: {
+		                inside: true,
+		                margin: 8
+		              },
+		              axisTick: {
+		                show: false
+		              },
+		              nameTextStyle: {
+		                color: "#000"
+		              },
+		              zlevel: 10,
+		              splitLine: {
+		                show: false
+		              },
+		              splitArea: {
+		                show: false
+		              }
+		            }
+		          ],
+		          series: [
+		            {
+		              name: data.legend[0],
+		              type: "bar",
+		              smooth: true,
+		              label: {
+		                normal: {
+		                  show: false
+		                }
+		              },
+		              data: data.seriesData[0]
+		            },
+		            {
+		              name: data.legend[1],
+		              type: "line",
+		              yAxisIndex: 1,
+		              smooth: true,
+		              data: data.seriesData[1],
+		              connectNulls: true
+		            }
+		          ]
+		        }
+		        return option
+		}
+
+
 		return service
 	})
